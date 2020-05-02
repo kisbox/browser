49e82241f65bb2e1717e619662af0b68525cf3aa
MisterTicot <mister.ticot@cosmic.plus>
2020-05-02 22:02:59 +0200

    logic Add View.helpers to handle definition states

--- src/view.helpers.js
+++ src/view.helpers.js
@@ -6,8 +6,45 @@
 /* Extension */
 const helpers = {}
 
-helpers.not = (any) => !any
-helpers.has = (any) => !!any
+/* Value */
+
+helpers.not = function (any) {
+  return !any || any instanceof Promise || any instanceof Error
+}
+
+helpers.has = function (any) {
+  return any && !(any instanceof Promise) && !(any instanceof Error)
+}
+
+helpers.truthy = function (any) {
+  return !!any && helpers.available(any)
+}
+
+helpers.available = function (any) {
+  return !(any instanceof Promise) && !(any instanceof Error)
+}
+
+helpers.notAvailable = function (any) {
+  return any instanceof Promise || any instanceof Error
+}
+
+/* States */
+
+helpers.pending = function (any) {
+  return any instanceof Promise
+}
+
+helpers.notPending = function (any) {
+  return !(any instanceof Promise)
+}
+
+helpers.failed = function (any) {
+  return any instanceof Error
+}
+
+helpers.notFailed = function (any) {
+  return !(any instanceof Error)
+}
 
 /* Exports */
 module.exports = helpers
